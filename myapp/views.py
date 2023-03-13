d22d9a499551822c4dff0141d3c476a027d59b8b
manishgupta248 <manishgupta248@gmail.com>
2023-03-13 21:10:01 +0530

    4 react frontend

--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html',{})
+
+def index(request):
+    return render(request,'frontend/index.html',{})
---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = (props) => {
+  return (
+    <div className="App">
+      <h1>Django React Setup Works- Jai Shri Ram</h1>
+    </div>
+  );
+};
+
+export default App;
